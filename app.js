e63ff4c9536960ca3c244c201e46dbb94c661ffd
Victor Landon <vlandon99@gmail.com>
2017-05-16 14:53:10 -0500

    Quick change

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use('/api/v1/', apiv1.router);
 var http = require('http');
 var server = http.createServer(app);
 server.listen(port, function () {
-    console.log('Weather Report listening on ' + url);
+    console.log('Weather Report listening at ' + url);
 });
 
 app.get("/", function(req, res) {
